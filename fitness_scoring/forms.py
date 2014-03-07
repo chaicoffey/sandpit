1a17b2fdff5e7023b046c3578df810e094f1a674
chaicoffey <coffey81@gmail.com>
2014-03-07 13:14:42 +1100

    Fixed Bug on add_student_form first_name

--- fitness_scoring/forms.py
+++ fitness_scoring/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class AddStudentForm(forms.Form):
     student_id = forms.CharField(max_length=30)
-    firstname = forms.CharField(max_length=100)
+    first_name = forms.CharField(max_length=100)
     surname = forms.CharField(max_length=100)
     gender = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')])
     dob = forms.DateField(input_formats=['%d/%m/%Y'], help_text='(dd/mm/yyyy)')
