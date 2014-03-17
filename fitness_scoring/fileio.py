ea38f43111be1608e6f3c5a4b417aa47e47b8bae
chaicoffey <coffey81@gmail.com>
2014-03-17 15:45:45 +1100

    Put in school list (no edit yet)

--- fitness_scoring/fileio.py
+++ fitness_scoring/fileio.py
@@ -1,6 +1,6 @@
 import csv
 import tempfile
-from fitness_scoring.models import create_student, update_student, create_teacher, update_teacher
+from fitness_scoring.models import create_student, update_student, create_teacher, update_teacher, create_school_and_administrator, update_school
 import os
 
 destination_directory = 'C:\\fitness_scoring_file_uploads\\'
@@ -66,6 +66,33 @@ def add_teachers_from_file(file_path_on_server, school_id):
             n_created += 1
         else:
             if update_teacher(check_name=False, first_name=first_name, surname=surname, school_id=school_id, username=username, password=password):
+                n_updated += 1
+            else:
+                n_not_created_or_updated += 1
+
+    return n_created, n_updated, n_not_created_or_updated
+
+
+def add_schools_from_file(file_path_on_server):
+    file_handle = open(file_path_on_server, 'rb')
+
+    dialect = csv.Sniffer().sniff(file_handle.read(1024))
+    dialect.strict = True
+
+    file_handle.seek(0)
+    schools_list_reader = csv.DictReader(file_handle, dialect=dialect)
+    # check headings are correct else throw exception
+
+    n_created = 0
+    n_updated = 0
+    n_not_created_or_updated = 0
+    for line in schools_list_reader:
+        (name, subscription_paid_text) = (line['name'], line['subscription_paid'])
+        subscription_paid = (subscription_paid_text == "Yes")
+        if create_school_and_administrator(name=name, subscription_paid=subscription_paid):
+            n_created += 1
+        else:
+            if update_school(name=name, subscription_paid=subscription_paid):
                 n_updated += 1
             else:
                 n_not_created_or_updated += 1
