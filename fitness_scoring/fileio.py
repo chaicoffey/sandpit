1799ffd309de8ccf713a845484b73deacd668228
chaicoffey <coffey81@gmail.com>
2014-03-19 11:33:49 +1100

    Put java script in forms for school list

--- fitness_scoring/fileio.py
+++ fitness_scoring/fileio.py
@@ -3,8 +3,8 @@ import tempfile
 from fitness_scoring.models import create_student, update_student, create_teacher, update_teacher, create_school_and_administrator, update_school
 import os
 
-#destination_directory = 'C:\\fitness_scoring_file_uploads\\'
-destination_directory = '/tmp/fitness_scoring_file_uploads'
+destination_directory = 'C:\\fitness_scoring_file_uploads\\'
+#destination_directory = '/tmp/fitness_scoring_file_uploads'
 
 
 def save_file(f):
--- fitness_scoring/forms.py
+++ fitness_scoring/forms.py
@@ -64,11 +64,6 @@ class EditClassForm(ClassForm):
     pass
 
 
-#class EditTeacherClassAllocationForm(forms.ModelForm):
-#    class Meta:
-#        model = TeacherClassAllocation
-
-
 class AddSchoolForm(forms.Form):
     name = forms.CharField(max_length=300)
     subscription_paid = forms.BooleanField(initial=False, required=False)
@@ -81,6 +76,46 @@ class AddSchoolForm(forms.Form):
 
     def get_subscription_paid(self):
         return self.cleaned_data['subscription_paid']
+
+    @staticmethod
+    def get_form_name():
+        return "addSchoolForm"
+
+    @staticmethod
+    def get_error_message_label_id_prefix():
+        return "addSchoolErrorMessageLabel_"
+
+    @staticmethod
+    def get_javascript_validation_call():
+        return "validateAddSchoolFields()"
+
+    @staticmethod
+    def get_javascript_validation_function():
+        return \
+            "function validateAddSchoolFields()\n\
+            {\n\
+            \n\
+                var form = document.forms['addSchoolForm'];\n\
+            \n\
+                var schoolName = form.elements['name'].value\n\
+                var schoolNameEntered = (schoolName != '')\n\
+                var schoolNameLettersValid = (schoolName.length >= 3) && (/^[a-zA-Z][a-zA-Z][a-zA-Z]/i.test(schoolName))\n\
+                var schoolNameValid = schoolNameEntered && schoolNameLettersValid\n\
+                if(!schoolNameValid) {\n\
+                    var schoolNameErrorMessage = document.getElementById('addSchoolErrorMessageLabel_name')\n\
+                    schoolNameErrorMessage.style.display = 'inherit'\n\
+                    if(!schoolNameEntered)\n\
+                        schoolNameErrorMessage.innerHTML = '- Please enter School Name';\n\
+                    else if(!schoolNameLettersValid)\n\
+                        schoolNameErrorMessage.innerHTML = '- Invalid School Name (first 3 characters must be alphabetic)';\n\
+                }\n\
+            \n\
+                var schoolSubscriptionPaid = form.elements['subscription_paid'].value\n\
+                var subscriptionPaidNameValid = true\n\
+            \n\
+                return schoolNameValid && subscriptionPaidNameValid;\n\
+            \n\
+            }\n"
 
 
 class EditSchoolForm(forms.Form):
@@ -100,3 +135,67 @@ class EditSchoolForm(forms.Form):
 
     def get_subscription_paid(self):
         return self.cleaned_data['subscription_paid']
+
+    @staticmethod
+    def get_form_name():
+        return "editSchoolForm"
+
+    @staticmethod
+    def get_modal_id():
+        return "editSchoolModal"
+
+    @staticmethod
+    def get_error_message_label_id_prefix():
+        return "editSchoolErrorMessageLabel_"
+
+    @staticmethod
+    def get_javascript_validation_call():
+        return "validateEditSchoolFields()"
+
+    @staticmethod
+    def get_javascript_show_modal_call():
+        return "showEditSchoolModal"
+
+    @staticmethod
+    def get_javascript_validation_function():
+        return \
+            "function validateEditSchoolFields()\n\
+            {\n\
+            \n\
+                var form = document.forms['editSchoolForm'];\n\
+            \n\
+                var schoolName = form.elements['name'].value\n\
+                var schoolNameEntered = (schoolName != '')\n\
+                var schoolNameLettersValid = (schoolName.length >= 3) && (/^[a-zA-Z][a-zA-Z][a-zA-Z]/i.test(schoolName))\n\
+                var schoolNameValid = schoolNameEntered && schoolNameLettersValid\n\
+                if(!schoolNameValid) {\n\
+                    var schoolNameErrorMessage = document.getElementById('editSchoolErrorMessageLabel_name')\n\
+                    schoolNameErrorMessage.style.display = 'inherit'\n\
+                    if(!schoolNameEntered)\n\
+                        schoolNameErrorMessage.innerHTML = '- Please enter School Name';\n\
+                    else if(!schoolNameLettersValid)\n\
+                        schoolNameErrorMessage.innerHTML = '- Invalid School Name (first 3 characters must be alphabetic)';\n\
+                }\n\
+            \n\
+                var schoolSubscriptionPaid = form.elements['subscription_paid'].value\n\
+                var subscriptionPaidNameValid = true\n\
+            \n\
+                return schoolNameValid && subscriptionPaidNameValid;\n\
+            \n\
+            }\n"
+
+    @staticmethod
+    def get_javascript_show_modal_function():
+        return \
+            "function showEditSchoolModal(school_pk, name, subscription_paid)\n\
+            {\n\
+            \n\
+                var modalForm = document.forms['editSchoolForm']\n\
+            \n\
+                modalForm.elements['school_pk'].value = school_pk\n\
+                modalForm.elements['name'].value = name\n\
+                modalForm.elements['subscription_paid'].checked = (subscription_paid == 'True');\n\
+            \n\
+                $('#editSchoolModal').modal('show');\n\
+            \n\
+            }\n"
