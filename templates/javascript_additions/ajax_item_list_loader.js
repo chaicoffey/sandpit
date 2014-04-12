1ab2ed5e4d509b1e8e6a02ac978a089e40816d25
chaicoffey <coffey81@gmail.com>
2014-04-12 11:59:45 +1000

    fixed list item number of sortable columns

--- templates/javascript_additions/ajax_item_list_loader.js
+++ templates/javascript_additions/ajax_item_list_loader.js
@@ -1,10 +1,13 @@
 
 // Loads school list tab and makes the list a dataTable (search functionality, etc.)
-function load_item_list(item_tab_id, item_table_id, item_list_url) {
+function load_item_list(item_tab_id, item_table_id, item_list_url, n_headings_to_exclude) {
+    var exclude_headings = new Array();
+    for (var back_index = 0; back_index < n_headings_to_exclude; back_index++)
+        exclude_headings[back_index] = -n_headings_to_exclude + back_index;
     $('#' + item_tab_id).load(base_url + item_list_url, function() {
         $('#' + item_table_id).dataTable( {
             "aoColumnDefs": [
-                { 'bSortable': false, 'aTargets': [ -2, -1 ] }
+                { 'bSortable': false, 'aTargets': exclude_headings }
             ]
         });
     });
--- templates/javascript_additions/ajax_school_list_loading.js
+++ templates/javascript_additions/ajax_school_list_loading.js
@@ -3,5 +3,5 @@ var base_url = window.location.protocol + "//" + window.location.host;
 
 // Load school list when page is first accessed.
 $(document).ready(function(){
-    load_item_list('school_list_tab', 'school_list', '/school/list/');
+    load_item_list('school_list_tab', 'school_list', '/school/list/', 2);
 });
