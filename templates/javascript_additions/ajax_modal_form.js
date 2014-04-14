79d805559d5c24810f943c37458fec189f62b645
chaicoffey <coffey81@gmail.com>
2014-04-14 15:02:40 +1000

    adjust to my computer

--- templates/javascript_additions/ajax_modal_form.js
+++ templates/javascript_additions/ajax_modal_form.js
@@ -3,26 +3,6 @@
 $(document).on('hidden.bs.modal', '#remoteModal', function() {
     $('#remoteModal').removeData('bs.modal');
 });
-/*
-//loads modal form window after submission from the modal window
-$(document).on('submit', '#modalForm', function(formEvent) {
-
-    formEvent.preventDefault();
-    var form = $('#modalForm');
-    $.ajax({
-        type: form.attr('method'),
-        url: form.attr('action'),
-        data: form.serialize(),
-        success: function(data, textStatus, jqXHR){
-            $("#remoteModal .modal-content").html(data);
-        },
-        error: function(data, textStatus, jqXHR){
-            $("#remoteModal .modal-content").html("An error occurred accessing modal form");
-        }
-    });
-
-});
-*/
 
 
 //loads modal form window after submission from the modal window
