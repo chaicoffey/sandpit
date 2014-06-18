4966ffe12375607a3de129559f03ea86e10e66c1
Mat <chai52@yahoo.com>
2014-06-18 11:57:03 +1000

    5.14.1 Modification. Added progress bar to student entry form

---
+++ templates/javascript_additions/student_entry_form_inner.js
@@ -0,0 +1,9 @@
+$(document).on('submit', '#studentEntryForm', function(formEvent) {
+
+    //formEvent.preventDefault();
+
+    //var form = $('#modalForm');
+    //var button_value = $('#modal_submit_button').attr('value');
+    $("#progress-bar").removeClass('hide');
+
+});
